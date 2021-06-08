592e6de6b9d6ec709d666816da88d2087fd972eb
G0Free <gyorfi.bence@gmail.com>
2021-06-08 23:06:38 +0200

    A Card Class, Thsi is an abstract class. Use it to inherit.

---
+++ CardHead/Card.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardHead
+{
+    enum CardType { Hearts, Tiles, Clovers, Pikes }
+    abstract class Card
+    {
+        int IDNumber;
+        CardType cardType;
+    }
+}
